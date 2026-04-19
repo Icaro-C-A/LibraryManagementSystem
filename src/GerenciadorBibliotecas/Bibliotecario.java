a0c268fe6a788b514b003153cdd5ffb2abf5d4ca
Ícaro <icarocostaalbuquerque@gmail.com>
2026-04-19 16:01:05 -0300

    feat: Implementa método de pagar multa com cálculo de troco e abatimento parcial

--- src/GerenciadorBibliotecas/Bibliotecario.java
+++ src/GerenciadorBibliotecas/Bibliotecario.java
@@ -5,4 +5,17 @@ public class Bibliotecario {
         Emprestimo emprestimo = new Emprestimo(codigo, livro, leitor);
         leitor.adicionarEmprestimo(emprestimo);
     }
+
+    public void pagarMulta(Leitor leitor, double valor){
+        if (valor > 0 && valor <= leitor.getDebito()){
+            leitor.setDebito(leitor.getDebito() - valor);
+            System.out.println("R$" + valor + " descontado do débito de " + leitor.getNome() + ". R$" + leitor.getDebito() + " de débito restante.");
+        }else if (valor > leitor.getDebito()){
+            double troco = valor - leitor.getDebito();
+            leitor.setDebito(0);
+            System.out.println("Todas multas foram pagas. Dar troco de R$" + troco);
+        }else{
+            System.out.println("O valor para pagar multa é inválido.");
+        }
+    }
 }
