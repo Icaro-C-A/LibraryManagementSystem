b0ddb5e208a7126af8a3cfa20fcb693eb5a8c1bb
Ícaro <icarocostaalbuquerque@gmail.com>
2026-04-24 14:26:26 -0300

    feat: add File creation method and Program class skeleton

--- src/Ficheiros/GerenciadorFicheiros.java
+++ src/Ficheiros/GerenciadorFicheiros.java
@@ -1,6 +1,17 @@
 package Ficheiros;
 import GerenciadorBibliotecas.*;
+import java.io.File;
 
 public class GerenciadorFicheiros {
-
+    private void criarFicheiro(String caminho, String nome){
+        File ficheiro = new File(caminho, nome);
+        try {
+            if (ficheiro.createNewFile()){
+                System.out.println("Ficheiro criado com sucesso:" + ficheiro.getName());
+            }
+        }
+        catch (Exception e){
+            System.out.println("Algo deu errado.");
+        }
+    }
 }
---
+++ src/GerenciadorBibliotecas/Programa.java
@@ -0,0 +1,8 @@
+package GerenciadorBibliotecas;
+import Ficheiros.GerenciadorFicheiros;
+
+public class Programa {
+    public static void main(String[] args) {
+
+    }
+}
