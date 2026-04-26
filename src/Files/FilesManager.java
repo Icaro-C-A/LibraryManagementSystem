3390b022fa33c0a99cb2f58bdd21e5a84947307a
Ícaro <icarocostaalbuquerque@gmail.com>
2026-04-26 15:04:52 -0300

    feat: add file constants and prepareFile method to FilesManager

--- src/Files/FilesManager.java
+++ src/Files/FilesManager.java
@@ -2,6 +2,11 @@ package Files;
 import java.io.File;
 
 public class FilesManager {
+    private final String COLLECTION_FILE = "collections.txt";
+    private final String READERS_FILE = "readers.txt";
+    private final String LOANS_FILE = "loans.txt";
+
+    //personalized methods
     private void createFile(String path, String name){
         File file = new File(path, name);
         try {
@@ -11,6 +16,19 @@ public class FilesManager {
         }
         catch (Exception e){
             System.out.println("Something went wrong.");
+        }
+    }
+
+    private boolean fileExists(String path, String name){
+        File file = new File(path, name);
+        return file.exists();
+    }
+
+    private void prepareFile(String path, String name){
+        if (!fileExists(path, name)){
+            createFile(path, name);
+        }else{
+            System.out.println("File " + path + name + " already exists.");
         }
     }
 }
