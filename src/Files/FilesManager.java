eca99485d9cc9120842f6f6eb5d3a660b7ca5cd4
Ícaro <icarocostaalbuquerque@gmail.com>
2026-04-26 15:33:31 -0300

    refactor: move storage files to a dedicated data folder

--- src/Files/FilesManager.java
+++ src/Files/FilesManager.java
@@ -2,16 +2,30 @@ package Files;
 import java.io.File;
 
 public class FilesManager {
+    private final String DATA_FOLDER = "data";
     private final String COLLECTION_FILE = "collections.txt";
     private final String READERS_FILE = "readers.txt";
     private final String LOANS_FILE = "loans.txt";
+
+    //constructor
+    public FilesManager() {
+        File directory = new File(DATA_FOLDER);
+        if (!directory.exists()){
+            directory.mkdir();
+            System.out.println("Created new directory: " + DATA_FOLDER);
+        }
+
+        prepareFile(DATA_FOLDER, COLLECTION_FILE);
+        prepareFile(DATA_FOLDER, READERS_FILE);
+        prepareFile(DATA_FOLDER, LOANS_FILE);
+    }
 
     //personalized methods
     private void createFile(String path, String name){
         File file = new File(path, name);
         try {
             if (file.createNewFile()){
-                System.out.println("File created successfully:" + file.getName());
+                System.out.println("File created successfully: " + file.getName());
             }
         }
         catch (Exception e){
